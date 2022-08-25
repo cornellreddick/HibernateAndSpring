68dd94a35585267886194b2a8ad1b19c7a4a4079
Cornell Reddick <cornellreddick@outlook.com>
2022-08-25 19:57:43 -0400

    Testeed dependency injection with Track Coach

--- src/com/springdemo/TrackCoach.java
+++ src/com/springdemo/TrackCoach.java
@@ -1,6 +1,13 @@
 package com.springdemo;
 
 public class TrackCoach implements Coach{
+
+    private FortuneService fortuneService;
+
+    public TrackCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "15 minutes of stretches, and run a 10 mile run.";
@@ -8,6 +15,6 @@ public class TrackCoach implements Coach{
 
     @Override
     public String getDailyFortune() {
-        return null;
+        return "Boss status " + fortuneService.getFortune();
     }
 }
