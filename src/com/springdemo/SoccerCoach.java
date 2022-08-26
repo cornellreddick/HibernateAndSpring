077aa7b1c3bf9f0d5ff5c6c0cd1d1f88c4f7422d
Cornell Reddick <cornellreddick@outlook.com>
2022-08-26 19:22:48 -0400

    Adding SoccerCoach Class

---
+++ src/com/springdemo/SoccerCoach.java
@@ -0,0 +1,13 @@
+package com.springdemo;
+
+public class SoccerCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return null;
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return null;
+    }
+}
